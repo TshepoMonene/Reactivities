c22b1fa59dcea14640a27941b2d207ce2b3a933d
Tshepo Monene <tmonene@SINGULAR.CO.ZA>
2023-07-06 08:21:42 +0200

    creating react app

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -12,4 +8,4 @@ namespace API.Controllers
     {
         
     }
-}
+} 
---
+++ API/demo.ts
@@ -0,0 +1,2 @@
+let n:string ='"f';
+n=","
