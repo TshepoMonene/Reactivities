6fa01b50d56e858988d73718c62d3a1d2f52a482
Tshepo Monene <tmonene@SINGULAR.CO.ZA>
2023-07-18 13:35:38 +0200

    End of section 6

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -15,9 +15,10 @@ interface Props {
     closeForm:()=>void;
     createOrEdit:(activity:Activity)=>void;
     deleteActivity :(id:string) => void;
+    submitting:boolean;
 }
 export default function ActivityDashboard({activities,selectedActivity,selectActivity,
-    cancelSelectActivity,editMode,openForm,closeForm, createOrEdit, deleteActivity}:Props  )
+    cancelSelectActivity,editMode,openForm,closeForm, createOrEdit, deleteActivity,submitting}:Props  )
 { 
     return( 
     <Grid>
@@ -26,6 +27,7 @@ export default function ActivityDashboard({activities,selectedActivity,selectAct
           activities={activities}
            selectActivity = {selectActivity}
            deleteActivity={ deleteActivity}
+           submitting={submitting}
            ></ActivityList>
         </Grid.Column>
         <Grid.Column width='6'>
@@ -35,7 +37,12 @@ export default function ActivityDashboard({activities,selectedActivity,selectAct
             cancelSelectActivity={cancelSelectActivity} 
             openForm={openForm}/> }
 
-            {editMode && <ActivityForm  activity={ selectedActivity} closeForm={closeForm}  createOrEdit={createOrEdit}/>}
+            {editMode &&
+             <ActivityForm 
+              activity={ selectedActivity} 
+              closeForm={closeForm}  
+              createOrEdit={createOrEdit}
+              submitting={submitting}/>}
         </Grid.Column>
     </Grid>)
    
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -6,8 +6,9 @@ interface Props{
     activity:Activity |undefined;
     closeForm:()=>void;
     createOrEdit:(activity:Activity) =>void;
+    submitting:boolean;
 }
-export default function ActivityForm({activity:selectedActivity,closeForm,createOrEdit}:Props){
+export default function ActivityForm({activity:selectedActivity,closeForm,createOrEdit,submitting}:Props){
     const initialState = selectedActivity ?? {
         id:'',
         title:'',
@@ -15,7 +16,7 @@ export default function ActivityForm({activity:selectedActivity,closeForm,create
         description:'',
         date:'',
         city:'',
-        venue:''
+        venue:'',
 
     }
     const [activity,setActivity] =useState(initialState);
@@ -24,20 +25,20 @@ export default function ActivityForm({activity:selectedActivity,closeForm,create
         createOrEdit(activity);
     }
     function handleInputChange(event: ChangeEvent<HTMLInputElement |HTMLTextAreaElement>){
-          const{name,value} =event.target;
+          const{name,value} = event.target;
           setActivity({...activity,[name]:value})   
     }
     return(
         <Segment clearing>
-            <Form onSubmit={handleSubmit} autoComplete='off' >
+            <Form onSubmit={handleSubmit} >
             <Form.Input placeholder='Title'  value={activity.title} name='title' onChange={handleInputChange}/>
-            <Form.TextArea placeholder='Description'  value={activity.description} name='Description' onChange={handleInputChange}/>
-            <Form.Input placeholder='Category' value={activity.category} name='Category' onChange={handleInputChange}/>
-            <Form.Input placeholder='Date' value={activity.date} name='Date' onChange={handleInputChange}/>
-            <Form.Input placeholder='City' value={activity.city} name='City' onChange={handleInputChange}/>
-            <Form.Input placeholder='Venue'  value={activity.venue} name='Venue' onChange={handleInputChange}/>
-             <Button floated='right' positive type='submit' content='Submit'  value={activity.title} name='title' onChange={handleInputChange}/>
-             <Button onClick={closeForm} floated='right' type='button' content='Cancel' value={activity.title} name='title' onChange={handleInputChange}/>
+            <Form.TextArea placeholder='Description'  value={activity.description} name='description' onChange={handleInputChange}/>
+            <Form.Input placeholder='Category' value={activity.category} name='category' onChange={handleInputChange}/>
+            <Form.Input type='date' placeholder='Date' value={activity.date} name='date' onChange={handleInputChange}/>
+            <Form.Input placeholder='City' value={activity.city} name='city' onChange={handleInputChange}/>
+            <Form.Input placeholder='Venue'  value={activity.venue} name='venue' onChange={handleInputChange}/>
+             <Button loading={submitting} floated='right' positive type='submit' content='submit'  value={activity.title} name='title' onChange={handleInputChange}/>
+             <Button onClick={closeForm} floated='right' type='button' content='cancel' value={activity.title} name='title' onChange={handleInputChange}/>
 
             </Form>
             
