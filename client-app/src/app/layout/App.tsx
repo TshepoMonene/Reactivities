80a7094edd0749bfc2d74c453e2462c50efd92a6
Tshepo Monene <tmonene@SINGULAR.CO.ZA>
2023-07-20 15:44:37 +0200

    Énd of section 8

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,32 +1,28 @@
-import { Fragment, useEffect, useState } from 'react';
+import { Fragment } from 'react';
 import {  Container} from 'semantic-ui-react';
 import NavBar from './NavBar';
-import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
-import agent from '../api/agent';
-import LoadingComponent from './LoadingComponents';
 import { useStore } from '../stores/store';
 import { observer } from 'mobx-react-lite';
+import { Outlet, useLocation } from 'react-router-dom';
+import HomePage from '../../features/home/HomePage';
 
 function App()  {
 
- const {activityStore} = useStore();
-
-  useEffect(()=>{
-    
-    agent.Activities.list().then(() =>{
-     activityStore.loadActivities();
-    })
-  },[activityStore])
+const location =useLocation();
 
 
-  if(activityStore.loadingInitial) return <LoadingComponent content='Loading app'/>
   return (
-    <Fragment>
+  <Fragment>
+    {location.pathname === '/' ?< HomePage/>:(
+
+     <>
    <NavBar ></NavBar>
    <Container style={{marginTop:'7em'}}>
 
-   <ActivityDashboard/>
+   <Outlet></Outlet>
    </Container>
+     </>
+    )}
    
     </Fragment>
   );
--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,15 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Button, Card, Icon, Image } from 'semantic-ui-react'
-import { Activity } from '../../../app/models/activity'
 import { useStore } from '../../../app/stores/store'
 import LoadingComponent from '../../../app/layout/LoadingComponents';
+import { observer } from 'mobx-react-lite';
+import { Link, useParams } from 'react-router-dom';
 
 
-export default function ActivityDetails(){
+export default observer(function ActivityDetails(){
   const {activityStore} = useStore();
-const {selectedActivity:activity} = activityStore;
+const {selectedActivity:activity,loadActivity,loadingInitial} = activityStore;
+const {id} =useParams();
 
-if(!activity) return <LoadingComponent/>;
+useEffect(()=>{
+  if(id) loadActivity(id);
+
+},[id,loadActivity])
+
+if( loadingInitial||!activity) return <LoadingComponent/>;
 
     return(
         <Card fluid>
@@ -25,11 +32,11 @@ if(!activity) return <LoadingComponent/>;
     </Card.Content>
     <Card.Content extra>
    <Button.Group widths='2'>
-     <Button  onClick={()=>activityStore.openForm(activity.id)}basic color='blue' content='Edit'/>
-     <Button onClick={activityStore.cancelSelectedActivity} basic color='grey' content='Cancel'/>
+     <Button as={Link} to={`/manage/${activity.id}`} basic color='blue' content='Edit'/>
+     <Button as={Link} to={'/activities'} basic color='grey' content='Cancel'/>
    
    </Button.Group>
     </Card.Content>
   </Card>
     )
-}
+  })
