a910087b60c8217da7926348b637649ecee444a4
Tshepo Monene <tmonene@SINGULAR.CO.ZA>
2023-09-01 14:37:22 +0200

    Mid section 22

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -3,7 +3,7 @@ import {  Container} from 'semantic-ui-react';
 import NavBar from './NavBar';
 import { useStore } from '../stores/store';
 import { observer } from 'mobx-react-lite';
-import { Outlet, useLocation } from 'react-router-dom';
+import { Outlet, ScrollRestoration, useLocation } from 'react-router-dom';
 import HomePage from '../../features/home/HomePage';
 import {ToastContainer} from 'react-toastify'
 import LoadingComponent from './LoadingComponents';
@@ -26,6 +26,7 @@ if(!commonStore.appLoaded) return <LoadingComponent content='Loading app...'/>
 
   return (
   <Fragment>
+    <ScrollRestoration />
   <ModalContainer />
  <ToastContainer position='bottom-right' theme='colored'/>
     {location.pathname === '/' ?< HomePage/>:(
